a401114a952849b250c4cff3295d3ac6eb2d6a56
Skelavik <Skwlavik@yandex.ru>
2024-04-02 09:17:28 +0300

    changes

--- Contest 3/A.py
+++ Contest 3/A.py
@@ -5,23 +5,25 @@ def math_car(k_n,a):
     res = 0
 
     for i in range(0,len(a)):
-
-        if (int(a[i]) - int(k_n[0]) + last) > 0:
-            res = int(a[i]) - int(k_n[0]) + last
-            last = int(a[i]) - int(k_n[0])
+        #print(a[i], last, k_n[0])
+        if ((int(a[i])+last) - int(k_n[0])) >= 0:
+            #print(a[i],last,k_n[0])
+            res = (int(a[i])+last) - int(k_n[0])
+            last = res
         else:
-            res = int(a[i]) - int(k_n[0]) + last
+            res = (int(a[i]) + last) - int(k_n[0])
             last = 0
 
-    print()
+    print(res)
 
 
-#l1 = input()
-l1 = "4 3"
+
+l1 = input()
+#l1 = "4 3"
 k_n = l1.split()
 
-#l2 = input()
-l2 = "3 4 5"
+l2 = input()
+#l2 = "3 4 5"
 a = l2.split()
 math_car(k_n,a)
 
